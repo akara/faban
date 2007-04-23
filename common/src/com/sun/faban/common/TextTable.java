327da5e5e176f4bbc6fc774f8d07d0f5c6d1d725
akara <akara>
2007-04-23 19:47:31 +0000

    Cleaned up the implementation: 1. Avoid modifications of the fields for alignment. 2. Avoid insertions. Insertions are slow. 3. Now field types are generic CharSequence to support a wide variety of char formats. 4. Supports formatting to StringBuffer, StringBuilder, or just Appendable. 5. Avoids a lot of space allocations.

--- common/src/com/sun/faban/common/TextTable.java
+++ common/src/com/sun/faban/common/TextTable.java
@@ -17,11 +17,13 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: TextTable.java,v 1.1 2007/04/21 07:17:08 akara Exp $
+ * $Id: TextTable.java,v 1.2 2007/04/23 19:47:31 akara Exp $
  *
  * Copyright 2005 Sun Microsystems Inc. All Rights Reserved
  */
 package com.sun.faban.common;
+
+import java.io.IOException;
 
 /**
  * The text table assists in formatting tabular data for text output.
@@ -37,7 +39,9 @@ public class TextTable {
                              "-----------------------------------------------";
     static final String FS = "  ";
 
-    StringBuilder[][] table;
+    static final String NULL_FIELD = "NULL";
+
+    CharSequence[][] table;
 
     /**
      * Constructs a text table with predefined number of rows and columns.
@@ -46,10 +50,7 @@ public class TextTable {
      */
     public TextTable(int rows, int columns) {
         // keep a row for the header;
-        table = new StringBuilder[++rows][columns];
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < columns; j++)
-                table[i][j] = new StringBuilder();
+        table = new CharSequence[++rows][columns];
     }
 
     /**
@@ -58,8 +59,16 @@ public class TextTable {
      * @param header The header
      */
     public void setHeader(int column, CharSequence header) {
-        table[0][column].setLength(0);
-        table[0][column].append(header);
+        table[0][column] = header;
+    }
+
+    /**
+     * Gets the header field's value.
+     * @param column The colund to get the header
+     * @return The current field value
+     */
+    public CharSequence getHeader(int column) {
+        return table[0][column];
     }
 
     /**
@@ -69,78 +78,125 @@ public class TextTable {
      * @param field The data field
      */
     public void setField(int row, int column, CharSequence field) {
-        table[++row][column].setLength(0);
-        table[row][column].append(field);
+        table[++row][column] = field;
     }
 
     /**
      * Obtains the field so formatters can insert values directly.
      * @param row The row index
      * @param column The column index
-     * @return The StringBuilder instance representing this field.
+     * @return The CharSequence representing this field.
      */
-    public StringBuilder getField(int row, int column) {
+    public CharSequence getField(int row, int column) {
         return table[++row][column];
     }
 
-    /**
-     * Formats the TextTable for output into a StringBuilder.
-     * @param b The buffer to output the text
-     * @return The same buffer
-     */
-    public StringBuilder format(StringBuilder b) {
+    private void _format(Appendable b) throws IOException {
+
+        // The size of each column
+        short[] colSize = new short[table[0].length];
+
         // Find the largest field
         // left-align first column.
         int maxWidth = 0;
-        for (int i = 0; i < table.length; i++)
+        for (int i = 0; i < table.length; i++) {
+            if (table[i][0] == null)
+                table[i][0] = NULL_FIELD;
             if (table[i][0].length() > maxWidth)
                 maxWidth = table[i][0].length();
-        // Left-align first column, right-pad it.
-        for (int i = 0; i < table.length; i++) {
-            int padWidth = maxWidth - table[i][0].length();
-            if (padWidth > 0)
-                table[i][0].append(PAD, 0, padWidth);
         }
-
+        colSize[0] = (short) maxWidth;
 
         // Find largest field in subsequent columns.
         for (int j = 1; j < table[0].length; j++) {
             maxWidth = 0;
-            for (int i = 0; i < table.length; i++)
+            for (int i = 0; i < table.length; i++) {
+                if (table[i][j] == null)
+                    table[i][j] = NULL_FIELD;
                 if (table[i][j].length() > maxWidth)
                     maxWidth = table[i][j].length();
-            // Right align
-            for (int i = 0; i < table.length; i++ ) {
-                int padWidth = maxWidth - table[i][j].length();
-                if (padWidth > 0)
-                    table[i][j].insert(0, PAD, 0, padWidth);
             }
+            colSize[j] = (short) maxWidth;
         }
 
-        // Now all fields should be just the right sicze. We can now dump
-        // it out. We do the headers first.
-        b.append(table[0][0]);
+        // Now we have the size of all columns. We can now dump
+        // it out. We do the headers first, appending first column and
+        // prepending the others.
+        b.append(table[0][0]).append(PAD, 0, colSize[0] - table[0][0].length());
         for (int j = 1; j < table[0].length; j++) {
-            b.append(FS).append(table[0][j]);
+            b.append(FS).append(PAD, 0, colSize[j] - table[0][j].length()).
+                    append(table[0][j]);
         }
         b.append('\n');
 
         // Then we do the line that separate headers.
-        b.append(LN, 0, table[0][0].length());
-        for (int j = 1; j < table[0].length; j++) {
-            b.append(FS).append(LN, 0, table[0][j].length());
+        b.append(LN, 0, colSize[0]);
+        for (int j = 1; j < colSize.length; j++) {
+            b.append(FS).append(LN, 0, colSize[j]);
         }
         b.append('\n');
 
         // Then all the table data.
         for (int i = 1; i < table.length; i++) {
-            b.append(table[i][0]);
+            b.append(table[i][0]).
+                    append(PAD, 0, colSize[0] - table[i][0].length());
             for (int j = 1; j < table[0].length; j++) {
-                b.append(FS).append(table[i][j]);
+                b.append(FS).append(PAD, 0, colSize[j] - table[i][j].length()).
+                        append(table[i][j]);
             }
             b.append('\n');
         }
         b.append('\n');
+    }
+
+    /**
+     * StringBuilder version of format. Same as format(Appendable) but
+     * does not throw exceptions.
+     * @param b The StringBuilder to append to
+     * @return The same StringBuilder
+     */
+    public StringBuilder format(StringBuilder b) {
+        try {
+            _format(b);
+        } catch (IOException e) {
+            // Never happens.
+        }
         return b;
+    }
+
+    /**
+     * StringBuffer version of format. Same as format(Appendable) but
+     * does not throw exceptions.
+     * @param b The StringBuffer to append to
+     * @return The same StringBuffer
+     */
+    public StringBuffer format(StringBuffer b) {
+        try {
+            _format(b);
+        } catch (IOException e) {
+            // Never happens.
+        }
+        return b;
+    }
+
+    /**
+     * Formats the TextTable and outputs to an Appendable.
+     * @param a The Appendable to output the text
+     * @return The same Appendable
+     * @exception IOException Problems writing to the Appendable
+     */
+    public Appendable format(Appendable a) throws IOException {
+        _format(a);
+        return a;
+    }
+
+    /**
+     * Returns the formatted table in a string.
+     *
+     * @return a formatted string representation of the table.
+     */
+    public String toString() {
+        StringBuilder b = new StringBuilder(8192);
+        return format(b).toString();
     }
 }
