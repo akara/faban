9e7ebd70494bc18f32b3e81915acb1da08cf3775
murlee <murlee>
2007-10-22 07:49:01 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    Updated fenxi to follow the newer command:
    fenxi process <Run Dir> <HTML Dir> <Run ID>
    and added the command to process compare.
    
    Added fenxi.cmd for process.
    
    Need to edit the Java files for compare yet.

--- harness/bin/fenxi.cmd
+++ harness/bin/fenxi.cmd
@@ -28,7 +28,7 @@ if not exist %JAVA% (
 
 REM TODO: Check file and rename, need to find way.
 
-set PROG=%FENXI_SCRIPTS/process
+set PROG=%FENXI_SCRIPTS/fenxi
 
 rem Check the PATHEXT and set it to the default if not exists
 if not defined PATHEXT set PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;
@@ -36,10 +36,23 @@ if not defined PATHEXT set PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.
 set CLASSPATH=%FENXI_HOME%\WEB-INF\lib\spark.jar;%FENXI_HOME%\WEB-INF\classes;%FENXI_HOME%\WEB-INF\lib\derby.jar;%FENXI_HOME%\WEB-INF\lib\jfreechart.jar;%FENXI_HOME%\WEB-INF\lib\jcommon.jar;%FENXI_HOME%\WEB-INF\lib\jruby.jar
 set PERL5LIB=%FENXI_HOME%\txt2db
 
+set JAVA_ARGS=-mx756 -Dderby.storage.pageReservedSpace=0 -Dderby.language.logQueryPlan=true -Dderby.storage.rowLocking=false -Dfenxi.basedir=%FENXI_HOME% -Djava.awt.headless=true -Dsun.java2d.pmoffscreen=false -Dfenxi.profile=default_profile
+
 deltree txt
 mkdir txt
 
-%JAVA% -Xmx756m -cp %CLASSPATH% -Dderby.storage.pageReservedSpace=0 -Dderby.language.logQueryPlan=true -Dderby.storage.rowLocking=false -Dfenxi.basedir=%FENXI_HOME% -Dfenxi.profile=default_profile -Djava.awt.headless=true -Dsun.java2d.pmoffscreen=false org.fenxi.cmd.process.ProcessRun %*
+set PROCESS=%1
+SHIFT
+
+REM Shift does not change the value in %*.
+if "%PROCESS%"=="process" (
+	%JAVA% -cp %CLASSPATH% %JAVA_ARGS% -Dfenxi.profile=%PROFILE% org.fenxi.cmd.process.ProcessRun %1 %2 %3
+)
+
+if "%PROCESS%"=="compare" (
+	%JAVA% -cp %CLASSPATH% %JAVA_ARGS% -Dfenxi.profile=%PROFILE% org.fenxi.cmd.compare.CompareDBinMemory %1 %2 %3
+)
+
 
 editXan() {
     sed 's%FenXi.jpg%/fenxi/html/FenXi.jpg%g' ${1} > ${1}.1
