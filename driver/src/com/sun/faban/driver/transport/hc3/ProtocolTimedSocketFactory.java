51a78cd8f4bb9910836937686d8d119c25581c23
akara <akara>
2009-06-30 06:32:40 +0000

    Faban-101: Provided the ApacheHC3Transport and timed socket fatories for this facility. Tests work out well but having some retry issues: I/O exception (org.apache.commons.httpclient.NoHttpResponseException) caught when processing request: The server <server> failed to respond. These did not happen when using the Sun transport. Issue is up for investigation.

---
+++ driver/src/com/sun/faban/driver/transport/hc3/ProtocolTimedSocketFactory.java
@@ -0,0 +1,83 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.sun.com/cddl/cddl.html or
+ * install_dir/legal/LICENSE
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at install_dir/legal/LICENSE.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: ProtocolTimedSocketFactory.java,v 1.1 2009/06/30 06:32:40 akara Exp $
+ *
+ * Copyright 2005 Sun Microsystems Inc. All Rights Reserved
+ */
+package com.sun.faban.driver.transport.hc3;
+
+import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import com.sun.faban.driver.transport.util.TimedSocket;
+
+/**
+ * The ProtocolTimedSocketFactory is used for the Apache Commons HttpClient 3.1
+ * to let the transport be timed.
+ *
+ * @author Akara Sucharitakul
+ */
+public class ProtocolTimedSocketFactory implements ProtocolSocketFactory {
+
+    public Socket createSocket(String host, int port, InetAddress localAddress,
+                               int localPort) throws IOException {
+        return new TimedSocket(host, port, localAddress, localPort);
+    }
+
+    public Socket createSocket(String host, int port, InetAddress localAddress,
+                               int localPort, HttpConnectionParams params)
+            throws IOException {
+        if (params == null) {
+            throw new IllegalArgumentException("Parameters may not be null");
+        }
+        int timeout = params.getConnectionTimeout();
+        if (timeout == 0) {
+            return createSocket(host, port, localAddress, localPort);
+        } else {
+            TimedSocket socket = new TimedSocket();
+            socket.bind(new InetSocketAddress(localAddress, localPort));
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            return socket;
+        }
+    }
+
+    public Socket createSocket(String host, int port) throws IOException {
+        return new TimedSocket(host, port);
+    }
+
+    /**
+     * All instances of ProtocolTimedSocketFactory are the same.
+     */
+    public boolean equals(Object obj) {
+        return ((obj != null) && obj.getClass().equals(getClass()));
+    }
+
+    /**
+     * All instances of ProtocolTimedSocketFactory have the same hash code.
+     */
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ driver/src/com/sun/faban/driver/transport/hc3/SecureProtocolTimedSocketFactory.java
@@ -0,0 +1,102 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.sun.com/cddl/cddl.html or
+ * install_dir/legal/LICENSE
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at install_dir/legal/LICENSE.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: SecureProtocolTimedSocketFactory.java,v 1.1 2009/06/30 06:32:40 akara Exp $
+ *
+ * Copyright 2005 Sun Microsystems Inc. All Rights Reserved
+ */
+package com.sun.faban.driver.transport.hc3;
+
+import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
+
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import com.sun.faban.driver.transport.util.TimedSocket;
+
+/**
+ * The SecureProtocolTimedSocketFactory is used for the Apache Commons
+ * HttpClient 3.1 SSL connections to let the transport be timed.
+ *
+ * @author Akara Sucharitakul
+ */
+public class SecureProtocolTimedSocketFactory implements SecureProtocolSocketFactory {
+
+    private static SSLSocketFactory secureFactory =
+            (SSLSocketFactory) SSLSocketFactory.getDefault();
+
+    public Socket createSocket(String host, int port, InetAddress localAddress,
+                               int localPort) throws IOException {
+        return secureFactory.createSocket(
+                new TimedSocket(host, port, localAddress, localPort),
+                host, port, true);
+    }
+
+    public Socket createSocket(String host, int port, InetAddress localAddress,
+                               int localPort, HttpConnectionParams params)
+            throws IOException {
+        if (params == null) {
+            throw new IllegalArgumentException("Parameters may not be null");
+        }
+        int timeout = params.getConnectionTimeout();
+        if (timeout == 0) {
+            return createSocket(host, port, localAddress, localPort);
+        } else {
+            TimedSocket socket = new TimedSocket();
+            socket.bind(new InetSocketAddress(localAddress, localPort));
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            return secureFactory.createSocket(socket, host, port, true);
+        }
+    }
+
+    public Socket createSocket(String host, int port) throws IOException {
+        return secureFactory.createSocket(new TimedSocket(host, port),
+                                            host, port, true);
+    }
+
+    public Socket createSocket(Socket socket, String host, int port,
+                               boolean close)
+            throws IOException, UnknownHostException {
+        if (socket instanceof TimedSocket)
+            return secureFactory.createSocket(socket, host, port, close);
+        else
+            throw new IllegalStateException(
+                    "Socket to use must already be a timed socket.");
+    }
+
+    /**
+     * All instances of SecureProtocolTimedSocketFactory are the same.
+     */
+    public boolean equals(Object obj) {
+        return ((obj != null) && obj.getClass().equals(getClass()));
+    }
+
+    /**
+     * All instances of SecureProtocolTimedSocketFactory have the same hash code.
+     */
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
