2fa3cd1327cb8eecd781f55053f884b1f4e7a1cf
akara <akara>
2009-08-27 15:39:19 +0000

    Faban-117: Provided test for isIpv4Address method.

--- common/test/com/sun/faban/common/UtilitiesTest.java
+++ common/test/com/sun/faban/common/UtilitiesTest.java
@@ -32,4 +32,21 @@ public class UtilitiesTest {
         assertTrue(test.equals(output));
         System.out.println(output);
     }
+
+    /**
+     * Test of isIpv4Address method, of class Utilities.
+     */
+    @Test
+    public void testIsIpv4Address() {
+        System.out.println("isIpv4Address");
+        assertTrue(Utilities.isIpv4Address("10.0.0.2"));
+        assertTrue(Utilities.isIpv4Address("129.10.12.51"));
+        assertTrue(Utilities.isIpv4Address("192.168.0.1"));
+        assertTrue(Utilities.isIpv4Address("224.224.224.224"));
+        assertTrue(Utilities.isIpv4Address("255.255.255.255"));
+        assertFalse(Utilities.isIpv4Address("256.255.255.255"));
+        assertFalse(Utilities.isIpv4Address("255.255.25b.255"));
+        assertFalse(Utilities.isIpv4Address("192.168.0"));
+        assertFalse(Utilities.isIpv4Address("192.168.0.15.2"));
+    }
 }
